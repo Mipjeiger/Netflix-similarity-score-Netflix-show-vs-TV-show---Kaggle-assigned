dcf0bfd9f3d3b127a1d6aee62baa912e6c707fdd
Mipjeiger <dianhinhin123@gmail.com>
2026-09-04 22:53:25 +0800

    Debugs on CD pipeline to refactor code within context file and push as true

--- src/models/recommend.py
+++ src/models/recommend.py
@@ -1,8 +1,9 @@
-import pandas as pd
+import pickle
+
 import numpy as np
+import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
-import pickle
 
 
 class ContentRecommender:
